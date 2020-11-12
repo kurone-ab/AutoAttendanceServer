a953dbfea2a37a011717bde8b5f9fba15b6781cc
AiRini <ygnaiih1680@mju.ac.kr>
2020-11-12 12:21:10 +0900

    time zone

--- auto_attendance/src/main/java/me/airini/auto_attendance/AutoAttendanceApplication.java
+++ auto_attendance/src/main/java/me/airini/auto_attendance/AutoAttendanceApplication.java
@@ -3,9 +3,13 @@ package me.airini.auto_attendance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AutoAttendanceApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")));
 		SpringApplication.run(AutoAttendanceApplication.class, args);
 	}
 }
